047c6e65c7125299f97b37dd9bb7b8fc239e54dc
Ramfaster <n1001ship@sit.com>
2019-12-03 17:36:24 +0900

    20191203 Rev05

--- flaskweb/flaskweb/__init__.py
+++ flaskweb/flaskweb/__init__.py
@@ -2,11 +2,19 @@
 The flask application package.
 """
 
-from flask import Flask
+from flask import Blueprint, Flask
 
 app = Flask(__name__)
 
+# main = Blueprint('main', __name__, url_prefix='/')
 # # 파일 이름이 index.py이므로
-from flaskweb.main.index import main as main
+# from flaskweb.index import main as main
+# app.register_blueprint(main)
 
-app.register_blueprint(main)
+from flaskweb.index import main as main_blueprint
+app.register_blueprint(main_blueprint, url_prefix='/')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "<h1>404 Error</h1>", 404
+
